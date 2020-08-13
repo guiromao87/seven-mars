c84c1216a212dd9ba1db64abf1ae529e35a6332f
Guilherme Romão <guilhermersimoes87@gmail.com>
2020-08-13 20:48:22 -0300

    initial commit

---
+++ src/main/java/com/guiromao/sevenmars/SevenMarsApplication.java
@@ -0,0 +1,13 @@
+package com.guiromao.sevenmars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenMarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevenMarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guiromao/sevenmars/SevenMarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiromao.sevenmars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenMarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
