63239e2acb37ff8e69726bf33d11a31341131539
Guilherme Romão <guilhermersimoes87@gmail.com>
2020-08-13 23:21:00 -0300

    add methods equals and hashcode to Probe

--- src/main/java/com/guiromao/sevenmars/model/Probe.java
+++ src/main/java/com/guiromao/sevenmars/model/Probe.java
@@ -1,5 +1,7 @@
 package com.guiromao.sevenmars.model;
 
+import java.util.Objects;
+
 public class Probe {
 
     private Position position;
@@ -13,4 +15,18 @@ public class Probe {
     public Position getPosition() { return position; }
 
     public String getDirection() { return direction.getDirectionName(); }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Probe probe = (Probe) o;
+        return Objects.equals(position, probe.position) &&
+                direction == probe.direction;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, direction);
+    }
 }
